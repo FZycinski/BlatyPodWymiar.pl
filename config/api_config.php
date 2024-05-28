8b90a230cb6587203202aa98e38bab2909442341
Filip Zycinski <sztylbas@gmail.com>
2024-05-28 13:05:20 +0200

    delete /config

--- config/api_config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'client_id' => '04216eb5e228458b912d608b48c77d32',
-    'client_secret' => '9e2pYwvK4YvR3p5rAv7wulw7hKP1oMLtSr3FFLiptsSWUetKLJiySvRisMuZ4X3D',
-    'redirect_uri' => 'https://test.blatypodwymiar.pl/views/allegro_order_view.php',
-];

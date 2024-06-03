43abe8476325156f9333f02087efb317b3120a63
Filip Zycinski <sztylbas@gmail.com>
2024-06-03 10:29:44 +0200

    Test

--- controllers/ShippingController.php
+++ controllers/ShippingController.php
@@ -3,17 +3,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-class ShippingController
-{
+class ShippingController {
     private $model;
 
-    public function __construct($model)
-    {
+    public function __construct($model) {
         $this->model = $model;
     }
 
-    public function createShippingLabel($accessToken, $shippingData)
-    {
+    public function createShippingLabel($accessToken, $shippingData) {
         $url = "https://api.allegro.pl/shipment-management/shipments/create-commands";
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -38,43 +35,18 @@ class ShippingController
         return json_decode($response, true);
     }
 
-    public function checkShipmentStatus($accessToken, $commandId)
-    {
-        $url = "https://api.allegro.pl/shipment-management/shipments/create-commands/$commandId";
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer $accessToken",
-            "Content-Type: application/vnd.allegro.public.v1+json",
-            "Accept: application/vnd.allegro.public.v1+json"
-        ));
-
-        $response = curl_exec($ch);
-        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-
-        if ($httpCode !== 200) {
-            exit("Failed to check shipment status. HTTP Code: $httpCode. Response: $response");
-        }
-
-        return json_decode($response, true);
-    }
-
-
     public function main() {
         if (isset($_GET["code"])) {
             $accessToken = $this->getAccessToken($_GET["code"]);
             
-            $orderId = 140; 
+            $orderId = 162; 
             $shippingData = $this->model->getShippingData($orderId);
 
             if ($shippingData) {
                 $labelResponse = $this->createShippingLabel($accessToken, $shippingData);
-                $commandId = $labelResponse['commandId'];
-
-                $statusResponse = $this->checkShipmentStatus($accessToken, $commandId);
+                print_r($labelResponse);
                 include('views/shipping_view.php');
+
             } else {
                 echo "No shipping data found for order ID: $orderId";
             }
@@ -83,15 +55,13 @@ class ShippingController
         }
     }
 
-    private function getAuthorizationCode()
-    {
+    private function getAuthorizationCode() {
         $authorization_redirect_url = AUTH_URL . "?response_type=code&client_id="
             . CLIENT_ID . "&redirect_uri=" . REDIRECT_URI . '&prompt=confirm';
         echo '<html><body><a href="' . $authorization_redirect_url . '">Zaloguj do Allegro</a></body></html>';
     }
 
-    private function getAccessToken($authorization_code)
-    {
+    private function getAccessToken($authorization_code) {
         $authorization = base64_encode(CLIENT_ID . ':' . CLIENT_SECRET);
         $headers = array("Authorization: Basic {$authorization}", "Content-Type: application/x-www-form-urlencoded");
         $content = "grant_type=authorization_code&code=" . urlencode($authorization_code) . "&redirect_uri=" . REDIRECT_URI;
@@ -114,3 +84,4 @@ class ShippingController
         return json_decode($tokenResult)->access_token;
     }
 }
+?>

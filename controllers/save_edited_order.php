f280a99b65ad49a078db3b369b73217d6fbc91ec
Filip Zycinski <sztylbas@gmail.com>
2024-06-03 14:07:09 +0200

    Test

--- controllers/save_edited_order.php
+++ controllers/save_edited_order.php
@@ -3,6 +3,7 @@ require_once '../config/DatabaseConnection.php';
 
 $mysqli = DatabaseConnection::getConnection();
 
+// Pobieranie danych z $_POST
 $access_token = $_POST['access_token'];
 $order_id = $_POST['order_id'];
 $order_status = $_POST['order_status'];
@@ -37,6 +38,10 @@ $source = $_POST['source'];
 $order_deadline = $_POST['order_deadline'];
 $comments = $_POST['comments'];
 $delivery_method_id = $_POST['delivery_method_id'];
+$package_length = $_POST['package_length'];
+$package_width = $_POST['package_width'];
+$package_height = $_POST['package_height'];
+$package_weight = $_POST['package_weight'];
 
 $phone_number_without_prefix = str_replace("+48", "", $delivery_address_phoneNumber);
 $phone_number_cleaned = preg_replace('/\s+/', '', $phone_number_without_prefix);
@@ -51,11 +56,11 @@ $mysqli->query("INSERT INTO orders (kind_of_wood, dimensions, is_varnished, is_o
 $order_id = $mysqli->insert_id;
 
 $stmt = $mysqli->prepare("INSERT INTO additional_order_data 
-    (access_token, order_id, order_status, order_payment_type, order_paid_amount, buyer_email, buyer_login, delivery_address_firstName, delivery_address_lastName, delivery_address_phoneNumber, delivery_address_street, delivery_address_city, delivery_address_zipCode, delivery_method_name, delivery_cost_amount, delivery_time_to, message_to_seller, item_id, item_name, item_quantity, item_price, invoice_address_street, invoice_address_zipCode, invoice_address_city, invoice_company_name, invoice_company_taxId, delivery_method_id) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    (access_token, order_id, order_status, order_payment_type, order_paid_amount, buyer_email, buyer_login, delivery_address_firstName, delivery_address_lastName, delivery_address_phoneNumber, delivery_address_street, delivery_address_city, delivery_address_zipCode, delivery_method_name, delivery_cost_amount, delivery_time_to, message_to_seller, item_id, item_name, item_quantity, item_price, invoice_address_street, invoice_address_zipCode, invoice_address_city, invoice_company_name, invoice_company_taxId, delivery_method_id, package_length, package_width, package_height, package_weight) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
 $stmt->bind_param(
-    "sissdssssssssssssssssssssss",
+    "sissdssssssssssssssssssssssdddd",
     $access_token,
     $order_id,
     $order_status,
@@ -82,7 +87,11 @@ $stmt->bind_param(
     $invoice_address_city,
     $invoice_company_name,
     $invoice_company_taxId,
-    $delivery_method_id
+    $delivery_method_id,
+    $package_length,
+    $package_width,
+    $package_height,
+    $package_weight
 );
 
 $stmt->execute();

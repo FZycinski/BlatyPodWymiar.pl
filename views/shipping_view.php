a972d6fd55088029448596b528646c7900dab64e
Filip Zycinski <sztylbas@gmail.com>
2024-06-03 10:25:47 +0200

    Test

--- views/shipping_view.php
+++ views/shipping_view.php
@@ -5,9 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shipping Data</title>
 </head>
-<body>
-    <pre>
-        <?php print_r($shippingData); ?>
-    </pre>
+<?php
+echo "<h1>Shipment Label Created</h1>";
+
+echo "<h2>Label Response:</h2>";
+echo "<pre>";
+print_r($labelResponse);
+echo "</pre>";
+
+echo "<h2>Shipment Status:</h2>";
+echo "<pre>";
+print_r($statusResponse);
+echo "</pre>";
+?>
 </body>
 </html>

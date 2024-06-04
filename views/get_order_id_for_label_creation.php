087b290b641287bc9fe611e9b4e8b734e628efd6
Filip Zycinski <sztylbas@gmail.com>
2024-06-04 15:17:14 +0200

    Test

--- views/get_order_id_for_label_creation.php
+++ views/get_order_id_for_label_creation.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <?php
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
+session_start();
+include_once("views/structure/header.php");
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: views/login_view.php");
+    exit;
+}
+?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Shipping Entry Point</title>
+    <title>Stwórz etykietę</title>
 </head>
 <body>
-    <h1>Wprowadź numery zamówień do wysłania</h1>
+    <h2>Wprowadź numery zamówień do wysłania</h2>
     <form action="../ShippingEntryPoint.php" method="post">
         <label for="order_ids">Numery zamówień (oddzielone przecinkami):</label><br>
         <textarea id="order_ids" name="order_ids" rows="1" cols="50"></textarea><br><br>

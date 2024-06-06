968564b044a46b7e5d96077b43d264913415f2b8
Filip Zycinski <sztylbas@gmail.com>
2024-06-06 12:03:20 +0200

    Test

--- views/order_form_view.php
+++ views/order_form_view.php
@@ -2,6 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['allFormData'])) {
     $orderedItems = json_decode($_POST['allFormData'], true);
 }
+var_dump($_POST['allFormData']);
 ?>
 
 <div>

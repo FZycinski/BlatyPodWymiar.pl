342d947347b562a8185a2a745dd60be8aa466787
Filip Zycinski <sztylbas@gmail.com>
2024-06-06 12:25:38 +0200

    Test

--- views/calculator_view.php
+++ views/calculator_view.php
@@ -74,7 +74,7 @@
     </form>
 </div>
 
-<script>
+    <script>
     document.getElementById("newFormButton").addEventListener("click", function(event) {
         createNewForm();
     });
@@ -228,12 +228,18 @@
             document.getElementById("result2" + formIndex).innerText = "Cena poza Allegro: " + priceWithDiscount.toFixed(2) + " zł.";
         }
         document.getElementById("result2" + formIndex).setAttribute('price_allegro2', priceWithDiscount.toFixed(2));
-        document.getElementById("result4" + formIndex).setAttribute('price_outside_allegro', totalPrice.toFixed(2));
-        document.getElementById("result5" + formIndex).setAttribute('price_total_outside_allegro', (totalPrice * piece).toFixed(2));
         if (!isNaN(totalPrice)) {
             document.getElementById("result3" + formIndex).innerText = "Do kupienia przez Allegro: " + Math.ceil(allegroPieces) + " szt.";
         }
         document.getElementById("result3" + formIndex).setAttribute('price_allegro3', Math.ceil(allegroPieces));
+        document.getElementById("result2" + formIndex).setAttribute('price_allegro2', priceWithDiscount.toFixed(2));
+document.getElementById("result4" + formIndex).setAttribute('price_outside_allegro', totalPrice.toFixed(2));
+document.getElementById("result5" + formIndex).setAttribute('price_total_outside_allegro', (totalPrice * piece).toFixed(2));
+
+// Dodajemy informacje o cenie do danych formularza
+var formData = new FormData(document.getElementById("priceCalculatorForm" + formIndex));
+formData.append('price_allegro', totalPrice.toFixed(2));
+formData.append('price_outside_allegro', totalPrice.toFixed(2));
 
 
         const elementsWithPrice1 = document.querySelectorAll('[price_allegro1]');
@@ -285,32 +291,31 @@
     }
 
     function submitForms() {
-        var formsData = [];
-        var forms = document.querySelectorAll('.calculatorForm');
+    var formsData = [];
+    var forms = document.querySelectorAll('.calculatorForm');
 
-        forms.forEach(function(form, index) {
-            var formData = new FormData(form);
-            var formObj = {};
-            formData.forEach(function(value, key) {
-                formObj[key] = value;
-            });
-
-            // Dodajemy również informacje o stanie checkboxów
-            var varnishChecked = document.getElementById("varnish" + index).checked;
-            var stainChecked = document.getElementById("stain" + index).checked;
-            var oilChecked = document.getElementById("oil" + index).checked;
-            var millChecked = document.getElementById("mill" + index).checked;
-
-            formObj['varnishChecked'] = varnishChecked;
-            formObj['stainChecked'] = stainChecked;
-            formObj['oilChecked'] = oilChecked;
-            formObj['millChecked'] = millChecked;
-
-            formsData.push(formObj);
+    forms.forEach(function(form, index) {
+        var formData = new FormData(form);
+        var formObj = {};
+        formData.forEach(function(value, key) {
+            formObj[key] = value;
         });
 
-        document.getElementById('allFormData').value = JSON.stringify(formsData);
-        document.getElementById('hiddenForm').submit();
-    }
+        var varnishChecked = document.getElementById("varnish" + index).checked;
+        var stainChecked = document.getElementById("stain" + index).checked;
+        var oilChecked = document.getElementById("oil" + index).checked;
+        var millChecked = document.getElementById("mill" + index).checked;
+
+        formObj['varnishChecked'] = varnishChecked;
+        formObj['stainChecked'] = stainChecked;
+        formObj['oilChecked'] = oilChecked;
+        formObj['millChecked'] = millChecked;
+
+        formsData.push(formObj);
+    });
+
+    document.getElementById('allFormData').value = JSON.stringify(formsData);
+    document.getElementById('hiddenForm').submit();
+}
 </script>
 </div>
